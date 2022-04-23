ba35e2c7ac9282c213962c29e476ec3bcdcd38f9
Vinícius Sarmento Costa Siqueira <vinicius.siqueira@quikdev.com.br>
2022-04-23 17:15:37 -0300

    Ajustando o login

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -14,7 +14,6 @@ module.exports = async (req, res, next) => {
 
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    console.log(decoded);
 
     req.userId = decoded.userId;
 
