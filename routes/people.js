7e5d201d964373599f90017c91a57cd5359e2c05
Vinícius Sarmento Costa Siqueira <vinicius.siqueira@quikdev.com.br>
2022-04-23 11:58:12 -0300

    Iniciando ajustes para logar na aplicação

--- routes/people.js
+++ routes/people.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const PeopleController = require("../src/app/controllers/PeopleController");
-const { PeopleModel } = require("../src/app/models/people");
+const models = require("../src/app/models/index");
 
-const controller = new PeopleController(PeopleModel);
+const controller = new PeopleController(models.people);
 
 router.get("/", controller.index.bind(controller));
 router.post("/", controller.store.bind(controller));
--- src/app/models/people.js
+++ src/app/models/people.js
@@ -1,21 +1,19 @@
 "use strict";
 const { Model } = require("sequelize");
-module.exports = {
-  PeopleModel: (sequelize, DataTypes) => {
-    class People extends Model {
-      static associate() {}
+module.exports = (sequelize, DataTypes) => {
+  class People extends Model {
+    static associate() {}
+  }
+  People.init(
+    {
+      name: DataTypes.STRING,
+      cpf: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "people",
+      tableName: "peoples",
     }
-    People.init(
-      {
-        name: DataTypes.STRING,
-        cpf: DataTypes.STRING,
-      },
-      {
-        sequelize,
-        modelName: "people",
-        tableName: "peoples",
-      }
-    );
-    return People;
-  },
+  );
+  return People;
 };
