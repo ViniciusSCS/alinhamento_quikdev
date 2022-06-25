2c41ea9c07d537a64f25d0def8ee448437d0f069
Vinícius Sarmento Costa Siqueira <vinicius.siqueira@quikdev.com.br>
2022-06-24 21:27:34 -0300

    Ajustes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,24 @@ module.exports = {
     es2021: true,
     node: true,
     jest: true,
-    "jest/globals": true,
+    'jest/globals': true
   },
-  extends: ["eslint:recommended", "prettier", "plugin:jest/recommended"],
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'prettier',
+    'plugin:jest/recommended',
+  ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
   },
-  ignorePatterns: ["test/**"],
-  plugins: ["prettier", "jest"],
+  ignorePatterns: ['test/**'],
+  plugins: ['prettier', 'jest'],
   rules: {
-    "prettier/prettier": "error",
-    "jest/no-disabled-tests": "warn",
-    "jest/no-focused-tests": "error",
-    "jest/no-identical-title": "error",
-    "jest/prefer-to-have-length": "warn",
-    "jest/valid-expect": "error",
-  },
-};
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  }
+}
--- routes/people.js
+++ routes/people.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../src/app/middlewares/auth");
-const PeopleController = require("../src/app/controllers/PeopleController");
-const models = require("../src/app/models/");
+const express = require('express');
+const router = express.Router()
+const authMiddleware = require('../src/app/middlewares/auth');
+const PeopleController = require('../src/app/controllers/PeopleController');
+const models = require('../src/app/models/');
 
-const controller = new PeopleController(models.people);
+const controller = new PeopleController(models.people)
 
-router.post("/", controller.store.bind(controller));
-router.post("/login", controller.login.bind(controller));
-router.post("/enviar_email");
-router.delete("/:id", controller.delete.bind(controller));
+router.post('/', controller.store.bind(controller))
+router.post('/login', controller.login.bind(controller))
+router.post('/enviar_email');
+router.delete('/:id', controller.delete.bind(controller))
 
-router.get("/me", authMiddleware, controller.me.bind(controller));
-router.get("/", authMiddleware, controller.index.bind(controller));
+router.get('/me', authMiddleware, controller.me.bind(controller))
+router.get('/', authMiddleware, controller.index.bind(controller))
 
-router.put("/atualizar_senha/:id", controller.updatePassword.bind(controller));
-router.put("/:id", authMiddleware, controller.update.bind(controller));
+router.put('/atualizar_senha/:id', controller.updatePassword.bind(controller))
+router.put('/:id', authMiddleware, controller.update.bind(controller))
 
-router.delete("/:id", authMiddleware, controller.delete.bind(controller));
+router.delete('/:id', authMiddleware, controller.delete.bind(controller))
 
-module.exports = router;
+module.exports = router
